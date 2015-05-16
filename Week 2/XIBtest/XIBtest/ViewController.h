b470f41cd5402a4e0b4be0b1f677eb726871cbbd
Carlos Butron <carlosbutron@icloud.com>
2015-05-16 20:44:02 +0200

    MVC, design patterns, Controllers, Views, Navigation, TableView, CollectionView, Segues and other concepts

---
+++ Week 2/XIBtest/XIBtest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XIBtest
+//
+//  Created by Carlos Butron on 12/05/15.
+//  Copyright (c) 2015 Carlos Butron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
