0fcf250c6074796cde123a141acc9e876534ffbb
Carlos Butron <carlosbutron@icloud.com>
2015-05-16 20:44:28 +0200

    MVC, design patterns, Controllers, Views, Navigation, TableView, CollectionView, Segues and other concepts

---
+++ Week 2/TakePhoto/TakePhoto/PicketViewController.h
@@ -0,0 +1,19 @@
+//
+//  PicketViewController.h
+//  TakePhoto
+//
+//  Created by Carlos Butron on 11/05/15.
+//  Copyright (c) 2015 Carlos Butron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+
+@interface PicketViewController : UIViewController
+
+
+
+
+
+@end
