131d4d89f2ecc46b44a381156f167da56de9a0fc
Carlos Butron <carlosbutron@icloud.com>
2015-05-16 20:31:26 +0200

    iOS Bootcamp pre-work

---
+++ Pre-work/Match-ismo-master/Matchismo/Deck.m
@@ -0,0 +1,54 @@
+//
+//  Deck.m
+//  Matchismo
+//
+//  Created by Carlos Butron on 15/04/15.
+//  Copyright (c) 2015 Carlos Butron. All rights reserved.
+//
+
+#import "Deck.h"
+
+@interface Deck()
+@property (strong, nonatomic) NSMutableArray *cards; // of Card
+@end
+
+@implementation Deck
+
+
+- (NSMutableArray *)cards
+{
+    if (!_cards) _cards = [[NSMutableArray alloc] init]; //lazy instantiation
+    return _cards;
+}
+
+
+-(void)addCard:(Card *)card atTop:(BOOL)atTop
+{
+    if(atTop){
+        [self.cards insertObject:card atIndex:0];
+    } else {
+        [self.cards addObject:card];
+    }
+    
+}
+
+- (void)addCard:(Card *)card
+{
+    [self addCard:card atTop:NO];
+}
+
+- (Card *)drawRandomCard{
+    
+    
+    Card *randomCard = nil;
+    
+    if ([self.cards count]) {
+        unsigned index = arc4random() % [self.cards count];
+        randomCard = self.cards[index];
+        [self.cards removeObjectAtIndex:index];
+    }
+
+    return randomCard;  
+}
+
+@end
---
+++ Pre-work/Match-ismo-master/README.md
@@ -0,0 +1,2 @@
+# Matchismo
+Stanford CS193p. Developing Applications for iOS. Fall 2013-14
