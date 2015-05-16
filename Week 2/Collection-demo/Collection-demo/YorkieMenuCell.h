b2f0f2fd9c2114acbe42eeb78df50f0f0bec8672
Carlos Butron <carlosbutron@icloud.com>
2015-05-16 20:43:07 +0200

    MVC, design patterns, Controllers, Views, Navigation, TableView, CollectionView, Segues and other concepts

---
+++ Week 2/Collection-demo/Collection-demo/YorkieMenuCell.h
@@ -0,0 +1,15 @@
+//
+//  YorkieMenuCell.h
+//  Collection-demo
+//
+//  Created by Carlos Butron on 15/05/15.
+//  Copyright (c) 2015 Carlos Butron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YorkieMenuCell : UICollectionViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *imageYorkieMenuCell;
+@property (weak, nonatomic) IBOutlet UILabel *labelYorkieMenuCell;
+
+@end
---
+++ Week 2/Collection-demo/Collection-demo/YorkieMenuCell.m
@@ -0,0 +1,13 @@
+//
+//  YorkieMenuCell.m
+//  Collection-demo
+//
+//  Created by Carlos Butron on 15/05/15.
+//  Copyright (c) 2015 Carlos Butron. All rights reserved.
+//
+
+#import "YorkieMenuCell.h"
+
+@implementation YorkieMenuCell
+
+@end
