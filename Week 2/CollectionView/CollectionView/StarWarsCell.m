8d5a8ad14de5e01fc597c0d23e097fae0368f09a
Carlos Butron <carlosbutron@icloud.com>
2015-05-16 20:45:23 +0200

    MVC, design patterns, Controllers, Views, Navigation, TableView, CollectionView, Segues and other concepts

---
+++ Week 2/CollectionView/CollectionView/StarWarsCell.m
@@ -0,0 +1,16 @@
+//
+//  MyCell.m
+//  Characters
+//
+//  Created by Carlos Butron on 14/05/15.
+//  Copyright (c) 2015 Carlos Butron. All rights reserved.
+//
+
+#import "StarWarsCell.h"
+
+@implementation StarWarsCell
+
+
+
+
+@end
---
+++ Week 2/CollectionView/CollectionView/WorldOf.h
@@ -0,0 +1,23 @@
+//
+//  WorldOf.h
+//  Characters
+//
+//  Created by Carlos Butron on 14/05/15.
+//  Copyright (c) 2015 Carlos Butron. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WorldOf : NSObject
+
+@property (nonatomic, strong) NSString *name;
+@property (nonatomic, strong) NSString *imageName;
+@property (nonatomic, strong) NSArray *characters;
+
+- (instancetype)initWithName:(NSString *)worldOfName
+                   imageName:(NSString *)worldOfImageName
+                   characters:(NSArray *)worldOfCharacters;
+
++ (NSArray *)setArrayWithWorldAndCharacters;
+
+@end
