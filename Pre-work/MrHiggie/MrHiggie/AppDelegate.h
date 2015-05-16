b5891471b81cbfc6a3547a13cd2aa189bf77a9b7
Carlos Butron <carlosbutron@icloud.com>
2015-05-16 20:31:58 +0200

    iOS Bootcamp pre-work

---
+++ Pre-work/MrHiggie/MrHiggie/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MrHiggie
+//
+//  Created by Carlos Butron on 08/04/15.
+//  Copyright (c) 2015 Carlos Butron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Pre-work/MrHiggie/MrHiggie/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MrHiggie
+//
+//  Created by Carlos Butron on 08/04/15.
+//  Copyright (c) 2015 Carlos Butron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
