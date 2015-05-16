040db319b14d8b7a424b79f9cd8f1cdcc3deae06
Carlos Butron <carlosbutron@icloud.com>
2015-05-16 20:34:59 +0200

    Objective-C basics, classes, initialization, protocols, delegates and other concepts

---
+++ Week 1/test/test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by Carlos Butron on 05/05/15.
+//  Copyright (c) 2015 Carlos Butron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
