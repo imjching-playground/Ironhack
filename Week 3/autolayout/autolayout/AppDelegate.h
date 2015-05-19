2c6fc165f8f5d8251b6206d20c3cbcdb7f87e62e
Carlos Butron <carlosbutron@icloud.com>
2015-05-19 16:23:19 +0200

    Drawing, Custom Views, Autolayout, Touches and Gestures

---
+++ Week 3/autolayout/autolayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  autolayout
+//
+//  Created by Carlos Butron on 19/05/15.
+//  Copyright (c) 2015 Carlos Butron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
