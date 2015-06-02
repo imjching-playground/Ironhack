d9080fd10d9a85946e40ea8047f2d0d8e007ecea
Carlos Butron <carlosbutron@icloud.com>
2015-06-02 16:31:10 +0200

    Control TextField position when Keyboard appear

---
+++ Week 5/TextFieldAndKeyboardControl/TexFieldAndKeyboardControl/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TexFieldAndKeyboardControl
+//
+//  Created by Carlos Butron on 02/06/15.
+//  Copyright (c) 2015 Carlos Butron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController<UITextFieldDelegate, UIPickerViewDataSource,UIPickerViewDelegate>
+
+
+@end
+
