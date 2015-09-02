49e0845a08ab58a9ef929dedbfc7ad9ae3f1b3a4
Carlos Butron <carlosbutron@icloud.com>
2015-09-03 01:40:24 +0200

    autolayout by code

---
+++ Week 3/AutoLayout by code/AutoLayout by code/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  AutoLayout by code
+//
+//  Created by Carlos Butron on 03/09/15.
+//  Copyright (c) 2015 Carlos Butron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UIView *black;
+
+
+@end
+
---
+++ Week 3/AutoLayout by code/AutoLayout by codeTests/AutoLayout_by_codeTests.m
@@ -0,0 +1,40 @@
+//
+//  AutoLayout_by_codeTests.m
+//  AutoLayout by codeTests
+//
+//  Created by Carlos Butron on 03/09/15.
+//  Copyright (c) 2015 Carlos Butron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface AutoLayout_by_codeTests : XCTestCase
+
+@end
+
+@implementation AutoLayout_by_codeTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
